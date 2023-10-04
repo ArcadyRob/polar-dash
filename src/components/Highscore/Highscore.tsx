70d5b23a2c324b447e627b1d0eab7f2f3288786b
Ruben Dijkstra <rubendijkstra1997@gmail.com>
2023-10-04 21:17:34 +0200

    fixed marquee with package

--- src/components/Highscore/Highscore.tsx
+++ src/components/Highscore/Highscore.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react';
 import { useEffect, useRef } from 'react';
+import Marquee from "react-fast-marquee";
 
 interface highscoreProps {
   highscores: {
@@ -10,34 +11,17 @@ interface highscoreProps {
 }
 
 function Highscore({ highscores }: highscoreProps) {
-  const containerRef = useRef<HTMLDivElement | null>(null);
-
-
-  useEffect(() => {
-    const container = containerRef.current!;
-
-    function scroll() {
-      container.style.transform = `translateX(${container.clientWidth}px)`;
-      requestAnimationFrame(scroll);
-    }
-
-    scroll();
-
-    return () => {
-      // Clean up any resources if needed
-    };
-  }, []);
 
   return (
     <div>
       <h2>Highscores</h2>
-      <div className="flex gap-4 overflow-x-scroll scrollbar-hide max-w-xs">
+      <Marquee className="flex max-w-xs">
         {highscores.map((score, index) => (
-          <div ref={containerRef} className="scrolling-text" key={index}>
-            <div className="w-max">{score.username}: {score.score}</div>
+          <div key={index}>
+            <div className="w-max px-2">{score.username}: {score.score}</div>
           </div>
         ))}
-      </div>
+      </Marquee>
     </div>
   );
 }
