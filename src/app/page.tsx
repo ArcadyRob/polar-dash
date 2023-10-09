168ae003f388684cef1a3f1b515e49b4ab5ee57c
Jaka Daneu <jakad@me.com>
2023-10-09 09:50:52 +0200

    fix: tailwind animations not working in storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image';
-
-import { BearSpinner } from '@/components';
-
 export default function Home() {
   return (
     <main
@@ -20,9 +16,6 @@ export default function Home() {
       >
         <h1 className="color--on-tertiary">PolarDash</h1>
         <p className="color--on-tertiary">Ready player one...</p>
-        <BearSpinner size="sm" />
-        <BearSpinner />
-        <BearSpinner size="lg" />
       </div>
     </main>
   );
