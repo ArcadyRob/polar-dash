37fb3bda745034ad910267f07829b4fb7d3c9ecb
Jaka Daneu <jakad@me.com>
2023-10-10 16:22:39 +0200

    story: adds spinner description

--- src/components/Spinner/BearSpinner.stories.tsx
+++ src/components/Spinner/BearSpinner.stories.tsx
@@ -5,14 +5,31 @@ import BearSpinner, { BearSpinnerProps } from './BearSpinner';
 export default {
   title: 'Spinners/BearSpinner',
   component: BearSpinner,
+  argTypes: {
+    size: {
+      description: 'The size of the spinner.',
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: 'A spinner with a polar bear head.',
+      },
+    },
+    layout: 'centered',
+  },
 } as Meta;
 
 export const Default: StoryFn<BearSpinnerProps> = args => (
   <BearSpinner {...args} />
 );
 
-export const Small: StoryFn<BearSpinnerProps> = () => <BearSpinner size="sm" />;
-export const Medium: StoryFn<BearSpinnerProps> = () => (
-  <BearSpinner size="md" />
+export const Small: StoryFn<BearSpinnerProps> = args => (
+  <BearSpinner {...args} size="sm" />
 );
-export const Large: StoryFn<BearSpinnerProps> = () => <BearSpinner size="lg" />;
+export const Medium: StoryFn<BearSpinnerProps> = args => (
+  <BearSpinner {...args} size="md" />
+);
+export const Large: StoryFn<BearSpinnerProps> = args => (
+  <BearSpinner {...args} size="lg" />
+);
